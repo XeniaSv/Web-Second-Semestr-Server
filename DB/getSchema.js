261aa25d8346efd3871c478037191b8f76492ca9
Xenia <savelicheva123@gmail.com>
2021-04-07 15:24:34 +0300

    Implement schema for DB

---
+++ DB/getSchema.js
@@ -0,0 +1,7 @@
+module.exports = function (mongoose) {
+    const Schema = mongoose.Schema;
+
+    const favouriteCity = new Schema({cityName: {type: "string", unique: true}}, {versionKey: false});
+
+    return mongoose.model("FavouritesCities", favouriteCity);
+}
