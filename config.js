ebc8fb42cd1e664b9969db9a39e834d6e7122695
Xenia <savelicheva123@gmail.com>
2021-04-09 15:13:35 +0300

    Correct files for deploy in heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    urlDB: "mongodb+srv://Xenia:qwerty12345678@cluster0.ssfb4.mongodb.net/WeatherForecast?retryWrites=true&w=majority",
-    PORT: "3000"
+    urlDB: process.env.MONGODB_URL,
+    PORT: process.env.PORT || 3000
 }
